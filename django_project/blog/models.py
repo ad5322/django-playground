31114ea163de8c9db0654dc02920b9ed48af849e
ad5322 <arundeepak966@gmail.com>
2024-10-22 22:08:23 +0530

    v2 edit, delet

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
-    content = models.TextField
+    content = models.TextField(null=True)
     published_at = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
 
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,55 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib import messages
 from .models import Post
+from .form import PostForm
 
+def delete_post(request,id):
+    post = get_object_or_404(Post,id = id)
+    context = {
+        'post':post,
+    }
+    if request.method == 'GET':
+        return render(request,'blog/post_confirm_delete.html',context)
+    elif request.method == 'POST':
+        post.delete()
+        messages.success(request,'The post has been deleted successfully')
+        return redirect('posts')
 
+def edit_post(request,id):
+    post = get_object_or_404(Post,id = id)
+    
+    if request.method == 'GET':
+        context = {
+            'form':PostForm(instance=post),
+            'id':id,
+            }
+        return render(request,'blog/post_form.html',context)
+    elif request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Post updated Successfully')
+            return redirect('posts')
+        else:
+            messages.error(request,'Please correct the following error')
+            return render(request,'blog/post_form.html',{'form':form,})
+
+def create_post(request):
+    if request.method == "GET":
+        
+        context = {'form':PostForm(),}
+        return render(request,'blog/post_form.html',context)
+    elif request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid:
+            form.save()
+            messages.success(request,'The post has been created successfully')
+            return redirect('posts')
+        else:
+            messages.error(request,'Please correct the follwing errors')
+            return render(request,'blog/post_form.html',{'form':form,})
+        
+        
 def home(request):
     posts = Post.objects.all()
    
